70e2d49831294d19e4d7373f24991650d8028902
HemantJadhav <129278150+JadhavHemant@users.noreply.github.com>
2024-07-16 22:43:10 +0530

    Method One Is Created

---
+++ src/test/java/GitDemo/Demo1.java
@@ -0,0 +1,11 @@
+package GitDemo;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test(priority=1)
+	public void MethodOne() {
+		System.out.println("Method One is Created");
+	}
+
+}
